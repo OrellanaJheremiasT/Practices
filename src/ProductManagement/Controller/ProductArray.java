1e326855b8e0050646acb929b634e82f0c4dbd97
OrellanaJheremiasT <d4rkr4in.ttv@gmail.com>
2025-11-06 23:08:19 -0500

    ProductArray

--- src/ProductManagement/Controller/ProductArray.java
+++ src/ProductManagement/Controller/ProductArray.java
@@ -4,31 +4,32 @@ import ProductManagement.Model.Product;
 import java.util.Scanner;
 
 public class ProductArray {
-    private Product[] products;
+    private final Product[] products;
 
     public ProductArray(int n){
         products = new Product[n];
     }
 
     public void addProduct(){
-        Scanner sc = new Scanner(System.in);
-        String name;
-        double price;
-        int units;
+        try (Scanner sc = new Scanner(System.in)) {
+            String name;
+            double price;
+            int units;
 
-        for (int i=0; i<products.length;i++){
-            System.out.println("Name:");
-            name=sc.nextLine();
-            System.out.println("Price:");
-            price= sc.nextDouble();
-            System.out.println("Units:");
-            units= sc.nextInt();
-            sc.nextLine();
+            for (int i=0; i<products.length;i++){
+                System.out.println("Name:");
+                name=sc.nextLine();
+                System.out.println("Price:");
+                price= sc.nextDouble();
+                System.out.println("Units:");
+                units= sc.nextInt();
+                sc.nextLine();
 
-            Product p1= new Product(name,price,units);
-            products [i]=p1;
+                Product p1= new Product(name,price,units);
+                products [i]=p1;
 
   
+            }
         }
     }
 
